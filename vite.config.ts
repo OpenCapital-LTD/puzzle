75747a534eff423f93ee3fe401293e319cc3a65a
Mc-shelton <sheltonnito@gmail.com>
2025-04-03 10:49:28 +0300

    reconfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  server: {
+    port: 3004,
+    // historyApiFallback: true, 
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), 
     },
   },
 });
+
