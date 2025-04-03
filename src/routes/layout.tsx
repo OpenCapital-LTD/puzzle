8dbb74658ec992a0fa295f8323c0ac34767fe69b
Mc-shelton <sheltonnito@gmail.com>
2025-04-03 17:17:56 +0300

    shared

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Cookies from 'js-cookie'
+import React, { useEffect } from 'react'
 import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
+    useEffect(()=>{
+        Cookies.remove("fun")
+        localStorage.removeItem('fun')
+    },[])
     return (
         <div className="app_container">
                 <Outlet />
